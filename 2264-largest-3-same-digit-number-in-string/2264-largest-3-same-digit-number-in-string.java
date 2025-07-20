ddad07d988840a0ae21b11fcb975a4f3e221f14a
Bisnu-ai <sambunu2003@gmail.com>
2025-07-20 21:49:03 +0530

    Time: 8 ms (24.16%), Space: 41.7 MB (96.65%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -3,7 +3,7 @@ class Solution {
     public String largestGoodInteger(String num) {
         int n=num.length();
 
-        char maxchar =0;
+        char maxchar =' ';
         for(int i=2;i<n;i++){
             if(num.charAt(i)==num.charAt(i-1) && num.charAt(i)==num.charAt(i-2)){
                if(num.charAt(i)>maxchar){
@@ -11,7 +11,7 @@ class Solution {
                }
             }
         }
-        if(maxchar == 0){
+        if(maxchar == ' '){
             return "";
         }
         return ""+maxchar+maxchar+maxchar;
