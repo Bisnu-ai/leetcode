387ac7a6ef4edeecba4b324facd9665cf73bf55d
Bisnu-ai <sambunu2003@gmail.com>
2025-07-22 19:17:04 +0530

    Time: 2 ms (97.87%), Space: 43.6 MB (60.74%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count=new int[26];
+    for(char ch:s.toCharArray()){
+        count[ch-'a']++;
+    }
+    for(char ch:t.toCharArray()){
+        count[ch-'a']--;
+    }
+   
+for(int a:count){
+    if(a!=0){ //if all element is zero in the count array then its anagram
+        return false;
+    }
+}
+return true;
+
+    }
+}
