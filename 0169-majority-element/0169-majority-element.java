70949defb16019f7dc67aeaf7f89678a24b5a338
Bisnu-ai <sambunu2003@gmail.com>
2026-01-10 02:56:33 +0530

    Time: 12 ms (35.51%), Space: 52.6 MB (85.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int majorityElement(int[] arr) {
-        //using bubble sort
-        int n=arr.length;
-        for(int x=0;x<n-1;x++){
-            boolean flag=true;
-            for(int i=0;i<n-1-x;i++){
-                if(arr[i]>arr[i+1]){
-                    int temp=arr[i];
-                    arr[i]=arr[i+1];
-                    arr[i+1]=temp;
-                    flag=false;
-                }
-            }
-        if(flag==true)break;
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+         int n=nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
         }
-        return arr[n/2];
+        int max = Integer.MAX_VALUE;
+        for (Integer k : mp.keySet()) {
+            if (mp.get(k) > n / 2) {
+                return k;
+            }
+        }
+        return -1;
     }
 }
