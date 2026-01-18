e880c280efdff9abe63d008a0d1673f802749b35
Bisnu-ai <sambunu2003@gmail.com>
2026-01-18 16:44:40 +0530

    Time: 8 ms (6.97%), Space: 48.2 MB (7.61%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -17,12 +17,13 @@ class Solution {
         int i = 0;
         int j = s.length() - 1;
         while (i <= j) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
-            } else {
+            if (s.charAt(i) != s.charAt(j)) {
+
         return check(s,i+1,j) || check(s,i,j-1);
-            }
+                
+            } 
+            i++;
+            j--;
         }
         return true;
     }
