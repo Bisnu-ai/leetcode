85cdd30380bbc7c6e770272cd6aa72fea6c43408
Bisnu-ai <sambunu2003@gmail.com>
2026-02-08 23:30:51 +0530

    Time: 0 ms (100%), Space: 48.4 MB (46.67%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] arr, int target) {
+      
+        int i=0;
+        int j= arr.length-1;
+     
+     while(i<=j){
+        int mid=i+(j-i)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+         if(arr[mid]<target){
+            i=mid+1;
+        }else{
+            j=mid-1;
+        }
+     }
+     return -1;
+       
+    }
+}
