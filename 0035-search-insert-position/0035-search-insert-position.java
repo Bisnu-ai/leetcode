5ae89dabea4231cb49d99c71f462c348ac3ab591
Bisnu-ai <sambunu2003@gmail.com>
2026-02-09 00:07:05 +0530

    Time: 0 ms (100%), Space: 44.2 MB (98.79%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+       int i=0;
+        int j= arr.length-1;
+     
+     while(i<=j){
+        int mid=i+(j-i)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+         if(arr[mid]<target){
+            i=mid+1;
+        }else{
+            j=mid-1;
+        }
+     }
+     return i;
+        
+    }
+}
