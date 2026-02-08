0a3a0a8ca344e0c0783eb94f2cb6e165dfe802af
Bisnu-ai <sambunu2003@gmail.com>
2026-02-09 01:51:38 +0530

    Time: 0 ms (100%), Space: 48.4 MB (10.74%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,33 @@
 class Solution {
     public int[] searchRange(int[] arr, int target) {
-       boolean isfirst;
-       int[]res=new int[2];
-      int first=bsearch(arr,target,true);
-              
-       int last=bsearch(arr,target,false);
-       res[0]=first;
-       res[1]=last;
-       
- return res;
-    }
-    public static int bsearch(int arr[],int target,boolean isfirst){
-          int left = 0;
-        
-    int right = arr.length - 1;
-    int ans=-1;
-    while (left <= right) {
-      int mid = left +(right-left) / 2;
-
-      if (arr[mid] == target) {
-        ans= mid;
-        if(isfirst){
-          right=mid-1;
-        }else{
-            left=mid+1;
+        int i=0;
+        int j=arr.length-1;
+        int f=-1;
+        int l=-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]==target){
+                f=mid;
+                j=mid-1;
+            }else if(arr[mid]<target){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
         }
-        
-      } else if (target > arr[mid]) {
-        left = mid+1; 
-      } else {
-        right = mid-1;
-      }
-
-    }
-return ans;
+        i=0;
+        j=arr.length-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]==target){
+                l=mid;
+                i=mid+1;
+            }else if(arr[mid]<target){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+        }
+        return new int[]{f,l};
     }
 }
