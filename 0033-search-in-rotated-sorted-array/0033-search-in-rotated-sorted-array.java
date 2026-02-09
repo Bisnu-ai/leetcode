dc4a220c0771e036d1371e12175cf3e4d4aa35bf
Bisnu-ai <sambunu2003@gmail.com>
2026-02-10 00:21:21 +0530

    Time: 0 ms (100%), Space: 44 MB (42.88%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,15 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        int n= arr.length;
-        
-        int first=0;
-        int end=n-1;
-       
-           while(first<=end){
+    public int search(int[] nums, int target) {
+      
 
-            int mid=first+(end-first)/2;
-              if(target==arr[mid]){
-                  return mid;
-              }
-               if(arr[0]<=arr[mid]){
-              if(target<arr[mid] && target>=arr[0]){
-                end=mid-1;
-              }else{
-                first=mid+1;
-              }
-           
-        }else{
-           
-            if(target>arr[mid] && target<=arr[end]){
-                first=mid+1;
-              }else{
-                end=mid-1;
-              }
-           }
+
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==target){
+            return i;
         }
-
-         return -1;  
-       
+       }
+         
+        
+        return -1;
     }
 }
