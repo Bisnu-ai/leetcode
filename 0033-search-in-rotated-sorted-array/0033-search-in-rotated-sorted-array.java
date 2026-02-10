134c038321a10c63368c6dca6d6f3c6b6eee9d30
Bisnu-ai <sambunu2003@gmail.com>
2026-02-10 21:30:09 +0530

    Time: 0 ms (100%), Space: 44.1 MB (24.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,60 +1,27 @@
 class Solution {
-    public int search(int[] nums, int target) {
-      
-    //=============O(n) soln==========
-
-    //  HashMap<Integer,Integer> hm=new HashMap<>();
-
-    //  for(int i=0;i<nums.length;i++){
-    //     hm.put(i,nums[i]);
-    //      if(hm.get(i)==target){
-    //         return i;
-    //     }
-    //  }
-     
-
-//=========O(logn)===============
+    public int search(int[] arr, int target) {
         int i=0;
-        int j=nums.length-1;
-        int res=-1;
+        int j=arr.length-1;
         while(i<=j){
             int mid=i+(j-i)/2;
-            if(nums[mid]==target){
-                i=mid+1;
-                res= mid;
-            }else if(nums[mid]<target){
-                i=mid+1;
-            }else{
-                j=mid-1;
-            }
-        }
-         
-         i=0;
-         j=nums.length-1;
-        while(i<=j){
-            int mid=i+(j-i)/2;
-            if(nums[mid]==target){
+            if(arr[mid]==target){
                 return mid;
             }
-             if(nums[mid]>=nums[i]){
-                if(nums[i]<=target && target<nums[mid]){
-                 j=mid-1;
+            if(arr[mid]>=arr[i]){
+                if(arr[i]<=target && arr[mid]>target){
+                    j=mid-1;
                 }else{
                     i=mid+1;
                 }
-            }else{
-                if(nums[mid]<target && target<=nums[j]){
-                 i=mid+1;
+            }
+            else{
+                if(arr[mid]<target && target<=arr[j]){
+                    i=mid+1;
                 }else{
                     j=mid-1;
                 }
-               
             }
         }
-         
-      
-         
-        
-        return res;
+        return -1;
     }
 }
